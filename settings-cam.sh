994965816f26c9b552c37cd4f70686abcfe1eb7c
isaie Trouffman Simonnet <trouffman@gmail.com>
2015-06-05 18:49:48 +0000

    adjusting settings for HWT2015

--- settings-cam.sh
+++ settings-cam.sh
@@ -1,10 +1,9 @@
-v4l2-ctl -d /dev/video0 --set-ctrl=contrast=155
-v4l2-ctl -d /dev/video0 --set-ctrl=white_balance_temperature_auto=155
-v4l2-ctl -d /dev/video0 --set-ctrl=white_balance_temperature=2800
-v4l2-ctl -d /dev/video0 --set-ctrl=sharpness=220
-v4l2-ctl -d /dev/video0 --set-ctrl=exposure_absolute=333
+v4l2-ctl -d /dev/video0 --set-ctrl=brightness=135
+v4l2-ctl -d /dev/video0 --set-ctrl=contrast=115
+v4l2-ctl -d /dev/video0 --set-ctrl=white_balance_temperature_auto=0
+v4l2-ctl -d /dev/video0 --set-ctrl=white_balance_temperature=4500
+v4l2-ctl -d /dev/video0 --set-ctrl=sharpness=170
 v4l2-ctl -d /dev/video0 --set-ctrl=exposure_auto_priority=1
 v4l2-ctl -d /dev/video0 --set-ctrl=focus_auto=0
-v4l2-ctl -d /dev/video0 --set-ctrl=contrast=200
-v4l2-ctl -d /dev/video0 --set-ctrl=contrast=180
-v4l2-ctl -d /dev/video0 --set-ctrl=focus_auto=1
+v4l2-ctl -d /dev/video0 --set-ctrl=led1_mode=3
+
