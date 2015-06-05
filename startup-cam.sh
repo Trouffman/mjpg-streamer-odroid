4a64dc8fd2852b4c1452a6d679edc1bc0d9c5dbc
Isaie Trouffman Simonnet <trouffman@gmail.com>
2015-06-05 18:13:12 +0930

    Script strat the webcam id 0 in 1080p 25fps over http.

--- startup-cam.sh
+++ startup-cam.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-./mjpg_streamer -i "./plugins/input_uvc/input_uvc.so -d /dev/video0 -f 15 -r 1920x1080" -o "./plugins/output_http/output_http.so -p 8081 -w ./www"
+./mjpg-streamer/mjpg_streamer -i "./mjpg-streamer/plugins/input_uvc/input_uvc.so -d /dev/video0 -f 225 -r 1920x1080" -o "./mjpg-streamer/plugins/output_http/output_http.so -p 8081 -w ./mjpg-streamer/www"
 
 
 
