305f57033ba8a96099c07847348ea157ffbdfaac
isaie Trouffman Simonnet <trouffman@gmail.com>
2015-06-05 18:23:02 +0000

    Version 1 pour HWT 2015

--- startup-cam.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-./mjpg-streamer/mjpg_streamer -i "./mjpg-streamer/plugins/input_uvc/input_uvc.so -d /dev/video0 -f 30 -r 1920x1080" -o "./mjpg-streamer/plugins/output_http/output_http.so -p 8081 -w ./mjpg-streamer/www"
-
-
-
