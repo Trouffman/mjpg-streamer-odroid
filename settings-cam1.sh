c31832b718b6b6490ee1d483f84b349e9e240617
Isaïe Trouffman Simonnet <trouffman@gmail.com>
2015-06-06 03:13:44 +0800

    Adding control settings for the second cam
    settings-cam1 to apply the setting to the second camera /dev/video1

---
+++ settings-cam1.sh
@@ -0,0 +1,9 @@
+v4l2-ctl -d /dev/video1 --set-ctrl=brightness=135
+v4l2-ctl -d /dev/video1 --set-ctrl=contrast=115
+v4l2-ctl -d /dev/video1 --set-ctrl=white_balance_temperature_auto=0
+v4l2-ctl -d /dev/video1 --set-ctrl=white_balance_temperature=4500
+v4l2-ctl -d /dev/video1 --set-ctrl=sharpness=170
+v4l2-ctl -d /dev/video1 --set-ctrl=exposure_auto_priority=1
+v4l2-ctl -d /dev/video1 --set-ctrl=focus_auto=0
+v4l2-ctl -d /dev/video1 --set-ctrl=led1_mode=3
+
